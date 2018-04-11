0b1b52eef711ee6c38b2f0b121d09ba2fde59b33
Daniel Bis <d.bis@wp.pl>
2018-04-11 17:52:13 -0400

    just reading the path and defined struct from slides

---
+++ fat.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+char fat_image[256];
+
+typedef struct {
+	unsigned char jmp[3];
+	char oem[8];
+	unsigned short sector_size;
+	unsigned char sectors_per_cluster;
+	unsigned short reserved_sectors;
+	unsigned char number_of_fats;
+	unsigned short root_dir_entries;
+	unsigned short total_sectors_short; // if zero, later field is used
+	unsigned char media_descriptor;
+	unsigned short fat_size_sectors;
+	unsigned short sectors_per_track;
+	unsigned short number_of_heads;
+	unsigned int hidden_sectors;
+	unsigned int total_sectors_long;
+
+	unsigned int bpb_FATz32;
+	unsigned short bpb_extflags;
+	unsigned short bpb_fsver;
+	unsigned int bpb_rootcluster;
+	char volume_label[11];
+	char fs_type[8];
+	char boot_code[436];
+	unsigned short boot_sector_signature;
+}_attribute((packed)) FAT32BootBlock;
+
+
+int main(int argc,char* argv[])
+{
+    int counter;
+    
+    if(argc==1)
+        printf("\nPlease, provide an image path name.\n");
+    if(argc==2)
+    {
+        strcpy(fat_image,argv[1]);
+    }
+
+    printf("pathname is: %s\n", fat_image);
+    return 0;
+}
